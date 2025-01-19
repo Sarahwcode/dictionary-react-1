da506215a5fe3913902e9673486a4af866496f49
Sarahwcode <sarahw181090@gmail.com>
2025-01-19 13:14:27 +0000

    made changes to meaning

--- src/Meanings.js
+++ src/Meanings.js
@@ -5,10 +5,10 @@ export default function Meanings(props) {
     return (
         <div>
         <h3>{props.meanings.partOfSpeech}</h3>
+       
         <p>{props.meanings.definition}</p>
+       
         <p>{props.meanings.example}</p>
-        <p>{props.meanings.synonyms}</p>
-        
         </div>
           
           
