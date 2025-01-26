89a1e76dce39c0bc09a62596740c0d7d4593f581
Sarahwcode <sarahw181090@gmail.com>
2025-01-26 11:52:32 +0000

    changes to phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Meanings from "./Meanings";
-import Phonetic from "./Phonetic";
+
 export default function Results (props)
 {
    
@@ -9,7 +9,7 @@ export default function Results (props)
     <div>
 <h2>{props.definition.word}</h2>
 
-<h2><Phonetic phonetic={props.definition.phonetic}/></h2>
+<h2>Phonetic:{props.definition.phonetic}</h2>
 
 {props.definition.meanings.map(function (meaning, index){
     return (
